5a43fdaaa215aaf5e1dc206dbfebc103a7d45f08
manoj-apare <manojapare@gmail.com>
2016-12-01 13:16:30 +0530

    File advanced search.

--- docroot/themes/extranet/js/file-search.js
+++ docroot/themes/extranet/js/file-search.js
@@ -1,8 +1,10 @@
 (function ($){
   $(document).ready(function () {
     views_exposed_filter = $('body.path-file-search .region-content div.view-filters.form-group');
+    tag_cloud = $('body.path-file-search .region-content section.block-dynamictagclouds ul.default_tag_clouds');
     $('#file-advanced-search').click(function(){
       views_exposed_filter.slideToggle();
+      tag_cloud.slideToggle();
       if($(this).hasClass('expanded')) {
         $(this).removeClass('expanded').html('Advanced Search');
       }
