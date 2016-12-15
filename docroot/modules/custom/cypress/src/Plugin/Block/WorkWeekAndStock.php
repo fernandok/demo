b8d00d7e8ade9a0164ab65e731244e23458b7779
manoj-apare <manojapare@gmail.com>
2016-12-15 17:23:50 +0530

    Fix work week date timezone issue.

--- docroot/modules/custom/cypress/src/Plugin/Block/WorkWeekAndStock.php
+++ docroot/modules/custom/cypress/src/Plugin/Block/WorkWeekAndStock.php
@@ -3,6 +3,7 @@
 namespace Drupal\cypress\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Component\Serialization\Json;
 
 /**
  * Provides a 'WorkWeekAndStock' block.
@@ -28,8 +29,12 @@ class WorkWeekAndStock extends BlockBase {
       $change = $data->results->row->change;
     }
     $build = [];
+    $user_time_zone = $this->getUserTimezone();
+    $date_time_zone = new \DateTimeZone($user_time_zone);
+    $date_time = new \DateTime('NOW', $date_time_zone);
     $build['work_week_and_stock'] = [
       '#theme' => 'cypress_ww_cy',
+      '#datetime' => $date_time->format("l d-F-Y g:i A"),
       '#ww' => date('y') . date('W'),
       '#cy_price' => $price,
       '#cy_change'  => $change,
@@ -37,6 +42,49 @@ class WorkWeekAndStock extends BlockBase {
     $build['#cache'] = ['max-age' => 0];
 
     return $build;
+  }
+
+
+  /**
+   * Method to get user timezone..
+   */
+  private function getUserTimezone() {
+    // Only do a check when the session variable is not set.
+    if (!isset($_SESSION['user_time_zone'])) {
+      $geoip_data = $this->getGeoipData();
+      if ($geoip_data['time_zone']) {
+        $_SESSION['user_time_zone'] = $geoip_data['time_zone'];
+      }
+      else {
+        $_SESSION['user_time_zone'] = date_default_timezone_get();
+      }
+    }
+    return $_SESSION['user_time_zone'];
+  }
+
+  /**
+   * Get user geo ip data.
+   */
+  private function getGeoipData() {
+    $user_ip = $this->getUserIp();
+    $url = 'http://freegeoip.net/json/' . $user_ip;
+    $request = \Drupal::httpClient()->get($url);
+    $response = $request->getBody();
+    $response_content = $response->getContents();
+
+    $data = Json::decode($response_content);
+    // If cannot determine the country code.
+    if ($data['type'] == 'error') {
+      return FALSE;
+    }
+    return $data;
+  }
+
+  /**
+   * Get user IP.
+   */
+  private function getUserIp() {
+    return \Drupal::request()->getClientIp();
   }
 
 }
