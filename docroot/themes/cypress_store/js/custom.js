198458cb9f61bbc71f9e1ab8f0ce0a022d33af4b
pravin <mrpkpravin@gmail.com>
2017-03-21 20:17:43 +0530

    slide responsive update

--- docroot/themes/cypress_store/js/custom.js
+++ docroot/themes/cypress_store/js/custom.js
@@ -1,9 +1,32 @@
 (function ($) {
     $('.view-delivery-address .view-content').slick({
-        infinite: true,
+        infinite: false,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: true
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 })(jQuery);
