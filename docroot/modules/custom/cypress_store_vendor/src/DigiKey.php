45f9c9a953ec835fcf7ffba7b5a282f310a2454d
saket <saketkumar2201@gmail.com>
2017-03-22 19:47:35 +0530

    DigiKey Query shipment

--- docroot/modules/custom/cypress_store_vendor/src/DigiKey.php
+++ docroot/modules/custom/cypress_store_vendor/src/DigiKey.php
@@ -4,8 +4,27 @@ namespace Drupal\cypress_store_vendor;
 
 
 class DigiKey {
-  public function QueryShipment($programId = 'cypress', $securityId = 'v0651sfp', $endPoint = 'http://test.samplecomponents.com/webservices/wssamples/service.asmx?wsdl'){
-    $url = new \SoapClient($endPoint);
+  /**
+   * @param string $programId
+   * @param string $securityId
+   * @param string $endPoint
+   */
+  public function QueryShipment(){
+    $endPoint = 'http://test.samplecomponents.com/webservices/wssamples/service.asmx?wsdl';
+    $parameters = array('program_id' => 'cypress', 'security_id' => 'v0651sfp');
+
+    $client = new \SoapClient($endPoint);
+    $response = $client->QueryShipments($parameters);
+    return $response;
+//    foreach($client->__getFunctions() as $predefinedFunctions){
+//      if($predefinedFunctions == ''){
+//
+//      }
+//      var_dump($predefinedFunctions);exit;
+//    }
+//    var_dump($client->__getFunctions());exit;
+
+
 
   }
 
