8bb147e8baa8078d69fdd7ef0846833d0e17b9b1
Subbu <subbu@valuebound.com>
2017-03-24 17:16:55 +0530

    added primary menu basic block

--- docroot/themes/cypress_store/js/main.js
+++ docroot/themes/cypress_store/js/main.js
@@ -2,9 +2,8 @@ var $ = jQuery;
 $(function(){
 	$(document).ready(function(){
 		$(".block-search-form-block").addClass("col-md-4");
-		$("#block-primarymenublock").addClass("col-md-4");
+		$("#block-primarymenu").addClass("col-md-4");
 		$('.language-menu, .account-menu').addClass("dropdown-menu");
-		// $(".language-menu").parent().find('li').addClass("dropdown","expanded");
 		$('.primary-menu li:nth-child(2), .primary-menu li:last-child').addClass('dropdown expanded');
 		$('.primary-menu li:nth-child(2), .primary-menu li:last-child').on('click',function(){
 			$(this).toggleClass('open');
