748127757f763eec1e2614a4680f654eaa421e60
chirag shah <chiragshah92@gmail.com>
2017-03-24 15:36:53 +0530

    new patch for js

--- docroot/modules/contrib/commerce_shipping/js/shipping_checkout.js
+++ docroot/modules/contrib/commerce_shipping/js/shipping_checkout.js
@@ -5,6 +5,25 @@
 (function ($, Drupal) {
     'use strict';
 
+    var existingValues = {};
+
+    function setExistingShippingValues(context) {
+        context = context || document;
+        var changed = false;
+        $('.required:not(.country)[data-drupal-selector^="edit-shipping-information-shipping-profile-"], [data-drupal-selector="edit-shipping-information-shipping-profile-profile-selection"]', context).each(function () {
+            var element = $(this);
+            var id = element.data('drupal-selector');
+            var val = element.val();
+
+            if (typeof existingValues[id] === 'undefined' || existingValues[id] !== val) {
+                existingValues[id] = val;
+                changed = true;
+            }
+        });
+
+        return changed;
+    }
+
     /**
      * Attaches the shipping recalculate behavior.
      *
@@ -14,20 +33,16 @@
      */
     Drupal.behaviors.commerceShippingRecalculate = {
         attach: function (context) {
-            if (!$('[id^="edit-shipping-information-shipments-"] .form-item', context).length) {
-                $('#edit-actions-next', context).prop('disabled', true);
+            if (!$('[data-drupal-selector^="edit-shipping-information-shipments-"]', context).length) {
+                $('[data-drupal-selector="edit-actions-next"]', context).prop('disabled', true);
             }
-            $('[id^="edit-shipping-information-"] .form-item', context).children('input.required').filter(':not(.country)').on('change keydown paste input', function () {
+
+            $('.required:not(.country)[data-drupal-selector^="edit-shipping-information-shipping-profile-"], [data-drupal-selector="edit-shipping-information-shipping-profile-profile-selection"]', context).on('change input', function () {
                 // just changed a field, clear any timer
                 window.clearTimeout(recalculateTimer);
                 // start the timer that will trigger recalculation after some inactivity
-                recalculateTimer = window.setTimeout(
-                    $.fn.commerceCheckShippingRecalculation,
-                    1500
-                );
+                recalculateTimer = window.setTimeout($.fn.commerceCheckShippingRecalculation, 1500);
             });
-
-
             $(window).load(function () {
                 $.fn.commerceCheckShippingRecalculation();
             });
@@ -35,13 +50,14 @@
             // Prefer to observe #edit-shipping-information-shipments, but ajax doesn't trigger an event for it
             var observer = new MutationObserver(function (mutations) {
                 mutations.forEach(function (mutation) {
-                    if (mutation.type == 'childList') { // showing and hiding the ajax indicator
+                    if (mutation.type === 'childList') { // showing and hiding the ajax indicator
                         if (mutation.addedNodes.length) {
-                            $('#edit-actions-next', context).prop('disabled', true);
-                        } else if (mutation.removedNodes.length) {
+                            $('[data-drupal-selector="edit-actions-next"]', context).prop('disabled', true);
+                        }
+                        else if (mutation.removedNodes.length) {
                             // check that shipping info is populated
-                            if ($('[id^="edit-shipping-information-shipments-"] .form-item', context).length) {
-                                $('#edit-actions-next', context).prop('disabled', false);
+                            if ($('[data-drupal-selector^="edit-shipping-information-shipments-"]', context).length) {
+                                $('[data-drupal-selector="edit-actions-next"]', context).prop('disabled', false);
                             }
                         }
                     }
@@ -65,59 +81,42 @@
      */
     $.fn.commerceCheckShippingRecalculation = function () {
         var recalculate = false;
-        var $shippingProfileSelectOptions = $('#edit-shipping-information-shipping-profile-profile-selection').find('option:selected');
-        if ($shippingProfileSelectOptions.length && $shippingProfileSelectOptions.val() != 'new_address') {
-            recalculate = true;
-        } else {
+        var $shippingProfileSelectOptions = $('[data-drupal-selector="edit-shipping-information-shipping-profile-profile-selection"]').find('option:selected');
+
+        recalculate = true;
+        if (!$shippingProfileSelectOptions.length || $shippingProfileSelectOptions.val() === 'new_address') {
             // validate minimum fields to calculate shipping
-            var dependentLocality = $('[id^="edit-shipping-information-"] .form-item .dependent-locality.required');
-            var dependentLocalityVal = null;
-            if (!dependentLocality.length) {
-                dependentLocalityVal = 'notrequired';
-            } else {
-                dependentLocalityVal = dependentLocality.val();
-            }
-            var locality = $('[id^="edit-shipping-information-"] .form-item .locality.required');
-            var localityVal = null;
-            if (!locality.length) {
-                localityVal = 'notrequired';
-            } else {
-                localityVal = locality.val();
-            }
-            var admArea = $('[id^="edit-shipping-information-"] .form-item .administrative-area.required');
-            var admAreaVal = null;
-            if (!admArea.length) {
-                admAreaVal = 'notrequired';
-            } else {
-                admAreaVal = admArea.val();
-            }
-            var postal = $('[id^="edit-shipping-information-"] .form-item .postal-code.required');
-            var postalVal = null;
-            if (!postal.length) {
-                postalVal = 'notrequired';
-            } else {
-                postalVal = postal.val();
-            }
-            if (dependentLocalityVal && localityVal && admAreaVal && (postalVal && postalVal.length > 4)) {
-                recalculate = true;
-            }
+            $.each([
+                'organization',
+                'dependent-locality',
+                'locality',
+                'administrative-area',
+                'postal-code'
+            ], function (index, field) {
+                var valid = true;
+                var $field = $('.required[data-drupal-selector="edit-shipping-information-shipping-profile-address-0-address-' + field + '"]');
+                if ($field.length && (!$field.val().length || ((field === 'postal-code' && $field.val().length <= 4)))) {
+                    valid = false;
+                    recalculate = false;
+                }
+                return valid;
+            });
         }
+
+        var changed = setExistingShippingValues();
 
         // Define the callback used with setTimeout to click the recalculation button
         // if there is ongoing AJAX operation.
-        var recalculateCallback = function () {
-            var $recalcButton = $('[id^="edit-shipping-information-recalculate"]');
-            if ($recalcButton.prop("disabled")) {
-                return setTimeout($.fn.commerceCheckShippingRecalculation, 100);
-            }
-
-            // Trigger the mousedown event on the shipping recalculation button.
-            $recalcButton.trigger('mousedown');
-        };
-
-        if (recalculate == true) {
-            return setTimeout(recalculateCallback, 100);
+        if (recalculate && changed) {
+            return setTimeout(function () {
+                var $recalcButton = $('[data-drupal-selector="edit-shipping-information-recalculate-shipping"]');
+                if ($recalcButton.prop('disabled')) {
+                    return setTimeout($.fn.commerceCheckShippingRecalculation, 100);
+                }
+                // Trigger the mousedown event on the shipping recalculation button.
+                $recalcButton.trigger('mousedown');
+            }, 100);
         }
-    }
+    };
 
 })(jQuery, Drupal);
