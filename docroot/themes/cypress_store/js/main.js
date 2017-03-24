6384fb0c9ecde38709e974e54e3d169c93cbb0ec
Subbu <subbu@valuebound.com>
2017-03-24 19:17:01 +0530

    primary menu custom block

--- docroot/themes/cypress_store/js/main.js
+++ docroot/themes/cypress_store/js/main.js
@@ -2,7 +2,7 @@ var $ = jQuery;
 $(function(){
 	$(document).ready(function(){
 		$(".block-search-form-block").addClass("col-md-4");
-		$("#block-primarymenu").addClass("col-md-4");
+		$("#block-primarymenublock").addClass("col-md-4");
 		$('.language-menu, .account-menu').addClass("dropdown-menu");
 		$('.primary-menu li:nth-child(2), .primary-menu li:last-child').addClass('dropdown expanded');
 		$('.primary-menu li:nth-child(2), .primary-menu li:last-child').on('click',function(){
