66e271178851fac6a4264ea032a7dfe56abc2270
Subbu <subbu@valuebound.com>
2017-04-17 12:36:57 +0530

    added search box to mobiles

--- docroot/themes/cypress_store/js/main.js
+++ docroot/themes/cypress_store/js/main.js
@@ -1,7 +1,8 @@
 var $ = jQuery;
 $(function(){
 	$(document).ready(function(){
-		$(".block-search-form-block").addClass('col-md-4 hidden-xs');
+		$(".block-search-form-block").addClass('col-md-4');
+		// $('input[type="search"]').parent().append('<input class="input-submit" type="submit">');
 		$("#block-primarymenublock").addClass('col-md-4 hidden-xs');
 		$('.language-menu, .account-menu').addClass("dropdown-menu");
 		$('.primary-menu li:nth-child(2), .primary-menu li:last-child').addClass('dropdown expanded');
