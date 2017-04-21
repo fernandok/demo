a5b619e3facd6f6ad0f35ec120484e3202ac404e
Subbu <subbu@valuebound.com>
2017-04-21 12:28:40 +0530

    changed delivery address  slides

--- docroot/themes/cypress_store/js/custom.js
+++ docroot/themes/cypress_store/js/custom.js
@@ -7,19 +7,40 @@
         arrows: true,
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 3000,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 1440,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 2,
                 }
             },
+            // {
+            //     breakpoint: 768,
+            //     settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //     }
+            // },
             {
                 breakpoint: 480,
                 settings: {
