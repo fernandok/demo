31382047030e53716c2465ba15f6550795fd9bde
chirag shah <chiragshah92@gmail.com>
2017-05-10 12:28:37 +0530

    demo feedback changes

--- docroot/modules/custom/cypress_custom_address/src/Plugin/views/field/CartRulesAdjustment.php
+++ docroot/modules/custom/cypress_custom_address/src/Plugin/views/field/CartRulesAdjustment.php
@@ -79,7 +79,7 @@ class CartRulesAdjustment extends FieldPluginBase {
           $adjustment_currency_code = $adjustment_amount->getCurrencyCode();
           $adjustment_currency = Currency::load($adjustment_currency_code);
           $currency_symbol = $adjustment_currency->getSymbol();
-          $cart_rules_adjustment .= '<div class="adjustment-amount"><span class = "adjustment-label">Discount Total: </span><span class = "adjustment-price">' . $currency_symbol . '' . $discount_price . '</span></div>';
+          $cart_rules_adjustment .= '<div class="adjustment-amount"><span class = "adjustment-label">Discount Total: </span><span class = "adjustment-price">' . $currency_symbol . ' ' . $discount_price . '</span></div>';
         }
       }
     }
