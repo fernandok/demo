99fc4ef94d55ba06b09452da78199dd17b89b612
rajeshwari <rajeshwari@valuebound.com>
2017-05-30 18:39:00 +0530

    formatted cypress order processor

--- docroot/modules/custom/cypress_custom_address/src/CypressOrderProcessor.php
+++ docroot/modules/custom/cypress_custom_address/src/CypressOrderProcessor.php
@@ -29,24 +29,22 @@ class CypressOrderProcessor implements OrderProcessorInterface {
       '2' => 'cypress_employees'
     );
     $check_roles = array_intersect($cypress_roles, $user_roles);
-    // $order->setAdjustments([]);
+    $order->setAdjustments([]);
     foreach ($order->getItems() as $order_item) {
-      $order_item->setAdjustments([]);
+     // $order_item->setAdjustments([]);
       $product_variation = $order_item->getPurchasedEntity();
       $default_product_variation_id = $order_item->getPurchasedEntityId();
       // $product_variation_type = $product_variation->get('type')->getValue()[0]['target_id'];
       if (!empty($product_variation)) {
-        $product_id = $product_variation->get('product_id')
-          ->getValue()[0]['target_id'];
+        $product_id = $product_variation->get('product_id')->target_id;
         $product = Product::load($product_id);
-        $product_type = $product->get('type')->getValue()[0]['target_id'];
+        $product_type = $product->get('type')->target_id;
         $variation_ids = $product->getVariationIds();
         $quantity = $order_item->getQuantity();
         $product_title = $product->getTitle();
         foreach ($variation_ids as $variation_id) {
           $variation_object = ProductVariation::load($variation_id);
-          $get_part_quantity = $variation_object->get('weight')
-            ->getValue()[0]['number'];
+          $get_part_quantity = $variation_object->get('weight')->number;
           $part_quantity = round($get_part_quantity);
           // The Part Quantity for Variant.
           $part_quantity = intval($part_quantity);
@@ -78,8 +76,7 @@ class CypressOrderProcessor implements OrderProcessorInterface {
          */
         // Access to CAT_B users purchasing CAT_A products.
         if ($product_type == 'part' && (in_array('authenticated', $user_roles) && !empty($check_roles) ? TRUE : FALSE)) {
-          $can_sample = $product->get('field_can_sample')
-            ->getValue()[0]['value'];
+          $can_sample = $product->get('field_can_sample')->value;
           $product_price = $order_item->getUnitPrice();
           $product_unit_price = $product_price->getNumber();
           // CAT_A products.
@@ -106,69 +103,80 @@ class CypressOrderProcessor implements OrderProcessorInterface {
 
         // Custom Promocode application.
         if ($product_type == 'part') {
-          $product_var = ProductVariation::load($default_product_variation_id);
-          $pro_title = $product_var->getTitle();
-          $promotion_id = get_promotion_id($pro_title);
-          $promotion = Promotion::load($promotion_id);
-          // Get the Coupon Code.
-          if (!empty($promotion)) {
-            $coupons = $promotion->getCouponIds();
-            foreach ($coupons as $coupon) {
-              $coupon_id = $coupon;
-              $coupon_obj = Coupon::load($coupon_id);
-            }
-            $usage_count = count_coupon_code($promotion_id);
-            if ($coupon_obj->getUsageLimit() > $usage_count) {
-                $offer = $promotion->get('offer')
-                  ->getValue()[0]['target_plugin_id'];
-                $promocode_amount = $promotion->get('offer')
-                  ->getValue()[0]['target_plugin_configuration']['amount'];
-                $product_id = $promotion->get('offer')
-                  ->getValue()[0]['target_plugin_configuration']['product_id'];
-
-                if ($offer == 'commerce_promotion_product_percentage_off') {
-                  if ($product_variation->getProductId() == $product_id) {
-                    $adjustment_amount = $order_item->getUnitPrice()
-                      ->multiply($promocode_amount);
-                    $promocode_adjustment_amount = $adjustment_amount->getNumber();
-                    // $adjustments = $item->getAdjustments();
-                    $promocode_adjustment[] = new Adjustment([
-                      'type' => 'cypress_promocode',
-                      'label' => 'Promocode Discount',
-                      'amount' => new Price('-' . $promocode_adjustment_amount, 'USD'),
-                    ]);
-                    $order_item->setAdjustments($promocode_adjustment);
-                    $order_item->save();
-                  }
-                }
-                elseif ($offer == 'commerce_promotion_product_fixed_off') {
-                  if ($product_variation->getProductId() == $product_id) {
-                    $unit_price = $order_item->getUnitPrice()->getNumber();
-                    if ($unit_price < $promocode_amount) {
-                      continue;
-                    }
-                    else {
-                      $discount_price = $promocode_amount;
-                    }
-                    $promocode_adjustment[] = new Adjustment([
-                      'type' => 'cypress_promocode',
-                      'label' => 'Promocode Discount',
-                      'amount' => new Price('-' . $discount_price, 'USD'),
-                    ]);
-                    $order_item->setAdjustments($promocode_adjustment);
-                    $order_item->save();
-                  }
-                }
-             // }
-            }
-          }
+          $this->cypressPromocodeApplication($order_item, $default_product_variation_id, $product_variation);
         }
       }
     }
   }
-}
- // Get the Promotion id based on product title
- function get_promotion_id($title) {
+
+
+  /**
+   * To Apply Promocode Adjustments for that specific product.
+   *
+   * @param \Drupal\commerce_order\Entity\OrderItem $order_item
+   *   The order_item entity.
+   * @param \Drupal\commerce_order\Entity\OrderItemInterface $default_product_variation_id.
+   *   The purchased entity id of that order_item.
+   * @param \Drupal\commerce_product\Entity\ProductVariation $default_product_variation_id
+   *   Product Variation entity.
+   */
+
+ protected function cypressPromocodeApplication($order_item, $default_product_variation_id, $product_variation) {
+   $product_var = ProductVariation::load($default_product_variation_id);
+   $pro_title = $product_var->getTitle();
+   $promotion_id = $this->getPromotionId($pro_title);
+   $promotion = Promotion::load($promotion_id);
+   // Get the Coupon Code.
+   if (!empty($promotion)) {
+     $coupons = $promotion->getCouponIds();
+     foreach ($coupons as $coupon) {
+       $coupon_id = $coupon;
+       $coupon_obj = Coupon::load($coupon_id);
+     }
+     $usage_count = $this->countCoupon($promotion_id);
+     if ($coupon_obj->getUsageLimit() > $usage_count) {
+       $offer = $promotion->get('offer')->target_plugin_id;
+       $promocode_amount = $promotion->get('offer')->target_plugin_configuration['amount'];
+       $product_id = $promotion->get('offer')->target_plugin_configuration['product_id'];
+       if ($offer == 'commerce_promotion_product_percentage_off') {
+         if ($product_variation->getProductId() == $product_id) {
+           $adjustment_amount = $order_item->getUnitPrice()
+             ->multiply($promocode_amount);
+           $promocode_adjustment_amount = $adjustment_amount->getNumber();
+           $promocode_adjustment = $order_item->getAdjustments();
+           $promocode_adjustment[] = new Adjustment([
+             'type' => 'cypress_promocode',
+             'label' => 'Promocode Discount',
+             'amount' => new Price('-' . $promocode_adjustment_amount, 'USD'),
+             'included' => TRUE
+           ]);
+           $order_item->setAdjustments($promocode_adjustment);
+           // $order_item->setQuantity(2);
+           $order_item->save();
+         }
+       }
+       elseif ($offer == 'commerce_promotion_product_fixed_off') {
+         if ($product_variation->getProductId() == $product_id) {
+           $unit_price = $order_item->getUnitPrice()->getNumber();
+           if ($unit_price > $promocode_amount) {
+             $discount_price = $promocode_amount;
+           }
+           $promocode_adjustment = $order_item->getAdjustments();
+           $promocode_adjustment[] = new Adjustment([
+             'type' => 'cypress_promocode',
+             'label' => 'Promocode Discount',
+             'amount' => new Price('-' . $discount_price, 'USD'),
+           ]);
+           $order_item->setAdjustments($promocode_adjustment);
+           $order_item->save();
+         }
+       }
+     }
+   }
+ }
+
+  // Get the Promotion id based on product title
+  protected function getPromotionId($title) {
 
     $query = \Drupal::database()->select('commerce_promotion_field_data', 'cp');
     $query->fields('cp', ['promotion_id']);
@@ -181,15 +189,18 @@ class CypressOrderProcessor implements OrderProcessorInterface {
     return $promotion_id;
   }
 
- // Get the count of promocode.
-function count_coupon_code($promotion_id) {
+  // Get the count of promocode.
+  protected function countCoupon($promotion_id) {
 
-  $query = \Drupal::database()->select('cypress_store_coupons', 'csc');
-  $query->fields('csc', ['coupon_code']);
-  $query->condition('csc.promotion_id', $promotion_id);
-  $results = $query->execute()->fetchAll();
-  $coupon_code = $results;
-  $usage_count = count($coupon_code);
+    $query = \Drupal::database()->select('cypress_store_coupons', 'csc');
+    $query->fields('csc', ['coupon_code']);
+    $query->condition('csc.promotion_id', $promotion_id);
+    $results = $query->execute()->fetchAll();
+    $coupon_code = $results;
+    $usage_count = count($coupon_code);
 
-  return $usage_count;
+    return $usage_count;
+  }
+
 }
+
